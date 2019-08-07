1890a8ae842994ce224bc59b97b2dfdccab65a9f
Nadeem Akhtar <42943800+akhtarnadeem915@users.noreply.github.com>
2019-08-07 20:11:12 +0530

    Create simple-newsletter.php

---
+++ php-codes/simple-newsletter.php
@@ -0,0 +1,29 @@
+<?php
+
+//creating a link between database and server
+//$link = mysqli_connect("hostname","db_user","db_passwd","db_name")
+$link = mysqli_connect("", "", "", "");
+// checking server - database connectivity and printing error if there
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+// on successfull conectivity it will return with this message
+echo "Success: A proper connection has established" . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+// handling unexpexted string error
+if(isset($_POST['name'])) {
+// getting data from form
+$name = mysqli_real_escape_string($link,$_POST['name']);
+$email = mysqli_real_escape_string($link,$_POST['email']);
+
+}
+// inserting data into mysql database
+$sql="INSERT INTO newsletter (name, email) VALUES ('$name', '$email')";
+
+//closing the connection
+mysqli_close($link);
+?>
